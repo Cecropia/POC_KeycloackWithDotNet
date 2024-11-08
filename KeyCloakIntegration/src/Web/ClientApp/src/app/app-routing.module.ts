00c11925d6575f40e2356108d78b1d2c8f01cf7e
Gonzalo Diaz <gdiaz@multiplied.com>
2024-11-08 08:28:45 -0500

    added keycloak-angular and related modules and cofigs

---
+++ KeyCloakIntegration/src/Web/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
+import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthGuard] },
+  { path: 'todo', component: TodoComponent, canActivate: [AuthGuard] }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes),],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
